17389888b1b8765833f9f2bdf165a4d329762020
Hadi-Abedini <hadi.z.abedini@gmail.com>
2025-01-14 13:51:11 +0330

    Refactor category and subcategory models to use concise schema definitions

--- src/modules/category/category.model.js
+++ src/modules/category/category.model.js
@@ -1,34 +1,30 @@
 const { Schema, Types, model } = require("mongoose");
 
-const SubCategorySchema = new Schema(
-  {
-    title: { type: String, required: true },
-    slug: { type: String, required: true, index: true },
-    items: {
-      type: [Object],
-      ref: "SubCategory",
-      required: false,
-      default: [],
-    },
+const SubCategorySchema = new Schema({
+  title: { type: String, required: true },
+  slug: { type: String, required: true, index: true },
+  items: {
+    type: [Object],
+    ref: "SubCategory",
+    required: false,
+    default: [],
   },
-);
+});
 
 const SubCategoryModel = model("SubCategory", SubCategorySchema);
 module.exports = SubCategoryModel;
 
-const CategorySchema = new Schema(
-  {
-    title: { type: String, required: true },
-    icon: { type: String, required: false },
-    image: { type: String, required: false },
-    items: {
-      type: [Object],
-      ref: "SubCategory",
-      required: false,
-      default: [],
-    },
+const CategorySchema = new Schema({
+  title: { type: String, required: true },
+  icon: { type: String, required: false, default: null },
+  image: { type: String, required: false, default: null },
+  items: {
+    type: [Object],
+    ref: "SubCategory",
+    required: false,
+    default: [],
   },
-);
+});
 
 const CategoryModel = model("Category", CategorySchema);
 module.exports = CategoryModel;
